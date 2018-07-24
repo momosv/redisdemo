450de488880cd20c56dca14c7f85c2d35259bfaa
linshengwen <momojy@vip.qq.com>
2018-07-24 16:45:27 +0800

    init 补充 login

--- src/main/java/cn/momosv/redisdemo/service/UserService.java
+++ src/main/java/cn/momosv/redisdemo/service/UserService.java
@@ -3,7 +3,7 @@ package cn.momosv.redisdemo.service;
 import org.springframework.cache.annotation.CachePut;
 import org.springframework.cache.annotation.Cacheable;
 
-public interface UserService {
+public interface UserService{
     @CachePut(value ="momou", key = "#p0")
     String set(String key);
     @Cacheable(value ="momou",key = "#p0")
